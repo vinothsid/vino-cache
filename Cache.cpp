833ef97851f1e8bd11f5048c7866fc83c010fe0c
Vinoth <vinothsid@gmail.com>
2012-02-05 03:11:03 -0500

    stable - single cache working fine as per validation runs

--- Cache.cpp
+++ Cache.cpp
@@ -4,8 +4,18 @@
 #include <fstream>
 #include <sstream>
 #include "string.h"
+#define DEBUG 0
 using namespace std;
 typedef unsigned int TAG;
+
+        long int numReads;
+        long int numReadMisses;
+	long int numReadHits;
+        long int numWrites;
+        long int numWriteMisses;
+	long int numWriteHits;
+        long int numWriteBacks;
+	long int numOpers;
 
 class Block {
 	bool valid;
@@ -96,8 +106,13 @@ Set::Set() {
 }
 
 void Set::print() {
+	char dirtyFlag;
 	for(int i=0;i<setAssoc;i++) {
-		cout << hex << blk[i].getTag() << ":" << blk[i].isDirty() << " " ;
+		if(blk[i].isDirty())
+			dirtyFlag = 'D';
+		else
+			dirtyFlag = ' ';
+		cout << "\t" << hex << blk[i].getTag() << " " << dirtyFlag   ;
 	}
 	cout << endl;
 }
@@ -105,8 +120,9 @@ void Set::print() {
 int Set::init(int assoc) {
 	setAssoc = assoc;
 	blk = new Block[assoc];
-	for(int i=0;i<setAssoc;i++) {
-		blk[i].setLru(i);
+	for(int i=1;i<=setAssoc;i++) {
+		blk[i-1].setLru(setAssoc-i);
+//		blk[i-1].setLru(0);
 	}
 }
 
@@ -136,26 +152,38 @@ int Set::place(TAG tag,char oper) {
 
 	if(placeIndex==-1) {
 		placeIndex = getLRU();
-
+		if(DEBUG)
+			cout << "victim: tag " << blk[placeIndex].getTag() ; 
 	} else {
+		if(DEBUG)
+			cout << "victim: none" << endl ;
         	blk[placeIndex].setValid(true);
 		
 	}
 
 	if (blk[placeIndex].isDirty() ) {
+		numWriteBacks++;
 		// issue a write to next level 
 		//nextLevel.write(tag);
-		cout << "Dirty block " << hex << blk[placeIndex].getTag() << " evicted" << endl;
+//		cout << "Dirty block " << hex << blk[placeIndex].getTag() << " evicted" << endl;
 		blk[placeIndex].setDirty(false);
-	}
+		if(DEBUG)
+			cout << ", dirty" << endl ;	
+	} 
+
 
 	if (oper == 'w') {
 		blk[placeIndex].setDirty(true);
+		if(DEBUG)
+			cout << " set dirty" << endl;
 	}
+
         blk[placeIndex].setTag(tag);
+	if(DEBUG)
+		cout << "Update LRU" << endl;
         updateRank(placeIndex);
 		
-
+	return 0;
 }
 
 int Set::updateRank(int i) {
@@ -177,16 +205,30 @@ bool Set::search(TAG tag,char oper) {
 			if(oper == 'w')
 				blk[i].setDirty(true);
 			updateRank(i);
+		
+			if(DEBUG) {
+				cout << "Hit" << endl;			
+				cout << "Update LRU" << endl;
+			}
 			return true;
 
 		}
 	}
 
+	if(DEBUG)
+		cout << "Miss" << endl;
 	//if not found in the entire set , its MISS
 /*	i = getLRU();
 	blk[i].setValid(true);
 	blk[i].setTag(tag);
 	updateRank(i);
+*/
+	//Collect stats
+
+/*	if(oper == 'r')
+		numReadMisses++;
+	else
+		numWriteMisses++;
 */
 	return false;
 }
@@ -241,28 +283,45 @@ class Cache {
 	int numSets;
 	int numOffsetBits;
 	int numSetsBits;
+	string cacheName;
 	unsigned int setsMask;
 	Set *s;
 	StreamBuffer *stBuf;
 	Cache *nextLevel;
-	
+// Stats variables
+/*
+	long int numReads;
+	long int numReadMisses;
+	long int numWrites;
+	long int numWriteMisses;
+	long int numWriteBacks;
+*/	
 public:
-	Cache(int blockSize,int numAssoc,int totalSize,int numStBufs,int numBlocksInStBuf,Cache *c);
+	Cache(int blockSize,int numAssoc,int totalSize,int numStBufs,int numBlocksInStBuf,Cache *c,string name);
 	int read(TAG addr); 
 	int write(TAG addr);
 	int fetchInstruction(char *fileName); // May not be necessary . read in the run function possible
 	int getIndexOfSet(TAG address); // From the whole address mask and return the index of set alone
 	int run(char *fileName); // Start the simulation
 	void printMembers();
+	void printContent();
+	void printStats();
 };
 
-Cache::Cache(int blockSize,int numAssoc,int totalSize,int numStBufs,int numBlocksInStBuf,Cache *c) {
+Cache::Cache(int blockSize,int numAssoc,int totalSize,int numStBufs,int numBlocksInStBuf,Cache *c,string name) {
 	blkSize = blockSize;
 	assoc = numAssoc;
 	cacheSize = totalSize;
 	numStreamBuf = numStBufs;
 	numSets = cacheSize/(blkSize * assoc);
 	numBlocksInStreamBuf = numBlocksInStBuf;
+
+        numReads=0;
+        numReadMisses=0;
+        numWrites=0;
+        numWriteMisses=0;
+        numWriteBacks=0;
+	cacheName = name;
 	s = new Set[numSets];
 
 	int i=0;
@@ -298,25 +357,53 @@ Cache::Cache(int blockSize,int numAssoc,int totalSize,int numStBufs,int numBlock
 	setsMask = ( numSets-1 ) << numOffsetBits;	
 }
 
+void Cache::printContent() {
+	for(int i=0;i<numSets;i++) {
+		cout << "Set\t" << dec <<  i <<":";
+		s[i].print();
+	}
+}
+
+void Cache::printStats() {
+        cout << "a. number of " <<  cacheName << " reads\t" << dec << numReads << endl;
+        cout << "b. number of " << cacheName << " read misses \t" << dec << numReadMisses << endl;
+        cout << "c. number of " << cacheName << " writes\t" << dec << numWrites << endl;
+        cout << "d. number of " << cacheName << " write misses\t" << dec << numWriteMisses << endl;
+        cout << "f. number of " << cacheName << " writebacks\t" << dec << numWriteBacks << endl;
+	cout << " number of " << cacheName << " read hits\t" << dec << numReadHits << endl;
+	cout << " number of " << cacheName << " write hits\t" << dec << numWriteHits << endl;
+}
+
 int Cache::read(TAG addr) {
 
-	unsigned int index = getIndexOfSet(addr);
+	int index = getIndexOfSet(addr);
 
 	unsigned int blockAddr = addr >> (numOffsetBits+numSetsBits);
-	if(s[index].search(blockAddr,'r')) {
-		cout<<"Read : In set index : " << index << ". Hit for : " << hex << blockAddr << endl;
-		s[index].print();
-		cout<<endl;
+	numReads++;
+	numOpers++;
 
+	if(DEBUG) {
+		cout << "----------------------------------------"<<endl;
+		cout << "# " << numOpers << " read " << hex << addr<< endl;
+		cout << cacheName << " read : " << hex << (addr >> numOffsetBits) << "(tag " <<  blockAddr << ", index " << dec << index << ")" << endl ;
+		
+	}
+	if(s[index].search(blockAddr,'r')) {
+	//	cout<<"Read : In set index : " << index << ". Hit for : " << hex << blockAddr << endl;
+	//	s[index].print();
+	//	cout<<endl;
+		numReadHits++;
 	} else {
-		cout<<"Read : In set index : " << index << ". Miss for : " << hex << blockAddr << endl;
+	//	cout<<"Read : In set index : " << index << ". Miss for : " << hex << blockAddr << endl;
 
 		// check in stream Buffer
 		// nextLevel->read(addr);
 //The following is simultated for retrieval from next level of cache or memory
+		numReadMisses++;
+	
 		s[index].place(blockAddr,'r');
-		s[index].print();
-		cout<<endl;
+	//	s[index].print();
+	//	cout<<endl;
 
 	}
 		 
@@ -328,21 +415,32 @@ int Cache::write(TAG addr) {
         unsigned int index = getIndexOfSet(addr);
 
         unsigned int blockAddr = addr >> (numOffsetBits+numSetsBits);
+	numWrites++;
+	numOpers++;
+
+	if(DEBUG) {
+		cout << "----------------------------------------"<<endl;
+		cout << "# " << numOpers << " write " << hex << addr<< endl;
+		cout << cacheName << " write : " << hex << (addr >> numOffsetBits) << "(tag " <<  blockAddr << ", index " << dec << index << ")" << endl ;
+
+	}
         if(s[index].search(blockAddr,'w')) {
-                cout<<"Write : In set index : " << index << ". Hit for : " << hex << blockAddr << endl;
-		//s[index].setDirty(true);
-                s[index].print();
-                cout<<endl;
+         //       cout<<"Write : In set index : " << index << ". Hit for : " << hex << blockAddr << endl;
+           //     s[index].print();
+           //     cout<<endl;
+		numWriteHits++;
 
         } else {
-                cout<<"Write : In set index : " << index << ". Miss for : " << hex << blockAddr << endl;
+                //cout<<"Write : In set index : " << index << ". Miss for : " << hex << blockAddr << endl;
 
                 // check in stream Buffer
                 // nextLevel->read(addr);
 //The following is simultated for retrieval from next level of cache or memory
+		numWriteMisses++;
                 s[index].place(blockAddr,'w');
-                s[index].print();
-                cout<<endl;
+             
+		//s[index].print();
+                //cout<<endl;
 
         }
 
@@ -412,6 +510,7 @@ int Cache::run(char * fileName) {
 			else {
 				cout << "Error : Invalid operation encountered" << endl;
 			}
+			memset(address,0,9);
 			
 		}
 		myfile.close();
@@ -423,7 +522,7 @@ int Cache::run(char * fileName) {
 
 void Cache::printMembers() {
 
-        cout<< "Block Size : " << blkSize << endl;
+/*        cout<< "Block Size : " << blkSize << endl;
         cout << "Assoc : " << assoc << endl;
         cout << "Cache Size : " << cacheSize << endl;
         cout << "Number of stream buffer : " << numStreamBuf << endl;
@@ -431,17 +530,31 @@ void Cache::printMembers() {
         cout << "Number of offset bits : " << numOffsetBits << endl ;
         cout << "Number of sets bits : " << numSetsBits << endl;
         cout << "Mask to get the set index : " << setsMask << endl;
+*/
 
+	cout << "===== Simulator configuration =====" << endl;
+	cout << "BLOCKSIZE:\t" << blkSize << endl;
+	cout << "L1_SIZE:\t" << cacheSize << endl;
+	cout << "L1_ASSOC:\t" << assoc << endl;
+	cout << "L1_PREF_N:             0" << endl;
+	cout << "L1_PREF_M:             0" << endl;
+	cout << "L2_SIZE:               0" << endl;
+	cout << "L2_ASSOC:              0" << endl;
+	cout << "L2_PREF_N:             0" << endl;
+	cout << "L2_PREF_M:             0" << endl;
+	cout <<  "trace_file:\t" << "gcc_trace.txt" << endl;
 }
 
 int main() {
 
-	Cache l1(16,2,1024,0,0,NULL) ;
+	Cache l1(16,2,1024,0,0,NULL,"L1") ;
 	l1.printMembers();
 
-	cout<< l1.getIndexOfSet(0x40007a64) << endl;
+//	cout<< l1.getIndexOfSet(0x40007a64) << endl;
 	l1.run("gcc_trace.txt");
-	cout<<"chk" << endl;
+	l1.printContent();
+	l1.printStats();
+//	cout<<"chk" << endl;
 
 }
 
